736e5b1790773d093e1c2b90fd0dc4f6c8530b52
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-08-13 14:42:46 -0400

    uploading now

--- access_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbhandle = sqlite3::open('tagarcadevids.db');
-$query = sqlite_query($dbhandle, 'SELECT title, header FROM videos LIMIT 5');
-while ($entry = sqlite_fetch_array($query, SQLITE_ASSOC)) {
-    echo 'title: ' . $entry['title'] . '  header: ' . $entry['header'];
-}
-?>
